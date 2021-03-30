be482761b4bf1bf30eec761d802286cf6d270f49
adityayaduvanshi0001 <79744911+adityayaduvanshi0001@users.noreply.github.com>
2021-03-31 00:00:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,19 @@ function compute()
   principal = document.getElementById("principal").value;
   rate = document.getElementById("rate").value;
   years = document.getElementById("years").value;
-  amount = parseInt((principal*rate*years)/100) + parseInt(principal);
+  amount = (principal*rate*years)/100;
   if(principal==""){
     alert("Please enter amount to compute interest.");
     onfocus.principal;
     return false;
   };
+  else if (principal<=0){
+    alert("Please check amount must be greater than zero.");
+    onfocus.principal;
+    return false;
+  }
   var Y = new Date();
-  var year = Y.getFullYear();
+  var year = parseInt(Y.getFullYear()) + parseInt(years);
   document.getElementById("result").innerHTML = "<br>If you deposit " + principal + ",<br>" + "at an interest rate of " + rate + "%.<br>" + "You will receive an amount of " + amount + ",<br>" + "in the year " + year + ".<br>"
   return true;
 }
