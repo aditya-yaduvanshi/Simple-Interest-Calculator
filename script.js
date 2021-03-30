b7bec777d7c026a2a2e6b9d21546dcd9c26dd408
adityayaduvanshi0001 <79744911+adityayaduvanshi0001@users.noreply.github.com>
2021-03-30 23:45:16 +0530

    add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function range()
+{
+  rate = document.getElementById("rate").value;
+  document.getElementById("spanrate").innerHTML = rate + "%";
+}
+function compute()
+{
+  principal = document.getElementById("principal").value;
+  rate = document.getElementById("rate").value;
+  years = document.getElementById("years").value;
+  amount = parseInt((principal*rate*years)/100) + parseInt(principal);
+  if(principal==""){
+    alert("Please enter amount to compute interest.");
+    onfocus.principal;
+    return false;
+  };
+  var Y = new Date();
+  var year = parseInt(Y.getFullYear()) + parseInt(years);
+  document.getElementById("result").innerHTML = "<br>If you deposit " + principal + ",<br>" + "at an interest rate of " + rate + "%.<br>" + "You will receive an amount of " + amount + ",<br>" + "in the year " + year + ".<br>"
+  return true;
+}
